b42a73e878560dcc30e3758eaf495922cba0d319
Evgen <ev.borodin1998@gmail.com>
2020-05-07 00:46:53 +0500

    Added 5-6 labs on the web. Part_6_4

--- part_6_4/js/task4.js
+++ part_6_4/js/task4.js
@@ -1,6 +1,3 @@
-//onClick
-//По нажатию на кнопку button_1 должно происходить закрытие окна документа.
-
 function img(){
     let picture = document.getElementById('image');
     let count = Math.ceil(Math.random()*5);
