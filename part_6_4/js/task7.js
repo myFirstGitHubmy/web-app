5268442375c1b32655064697b9e0ab22611b6a41
Evgen <ev.borodin1998@gmail.com>
2020-05-07 18:09:29 +0500

    Added new seven lab: html, css, js

---
+++ part_6_4/js/task7.js
@@ -0,0 +1,10 @@
+
+let inputs = document.querySelectorAll('input[data-rule]');
+console.log(inputs);
+
+for (let input of inputs){
+    input.addEventListener('blur', function(){
+        let rule = this.dataset.rule;
+        console.log(rule);
+    });
+}
